e89a4f8455645710b7c74862e806b9ac6bb55073
AMOL CHHABRA <amolchhabra@AMOLs-MacBook-Air.local>
2022-06-05 20:43:33 +0530

    Version 2

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Sucess..!')
-  res.json({ message: 'Hello, Azure.' })
+  res.json({ message: 'Hello, Azure Version 2' })
 });
 
 
