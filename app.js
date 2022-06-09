4af7569f46a8c667d7da2247e303c4ae88f2337b
kakarot291994 <amolchh1994@gmail.com>
2022-06-09 07:54:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.get('/', (req, res) => {
   console.log('Sucess..!')
-  res.json({ message: 'Hello, Azure Version 3' })
+  res.json({ message: 'Hello, Azure Version 4' })
 });
 
 
